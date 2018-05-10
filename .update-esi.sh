0dc60ad4c7df9875000a64705679dbf1833efddd
root <root@ip-172-31-57-248.ec2.internal>
2018-05-10 18:13:00 +0000

    v0.8.1 swagger-2.3.1

---
+++ .update-esi.sh
@@ -0,0 +1,10 @@
+rm -rfv *
+rm -rfv .php_cs .travis.yml .swagger-codegen .swagger-codegen-ignore
+read -p "Path to swagger codegen jar relative to this dir:" swagger
+read -p "Git Commit Message (eg. 'v<ccp-api-version> swagger-<swagger-codegen-version>'):" msg
+java -jar $swagger generate -i https://esi.evetech.net/latest/swagger.json?datasource=tranquility -l php --git-repo-id "esi-php-client" --git-user-id "jbs1"  --release-note "$msg"
+mv SwaggerClient-php/.[!.]* .
+mv SwaggerClient-php/* .
+rm -rfv SwaggerClient-php
+sudo chmod +x git_push.sh
+sudo ./git_push.sh
